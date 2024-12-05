44535616004b1211f4dec5616a16fec8c550dbec
Julian Carvajal <nettoingenieria@gmail.com>
2024-12-04 21:00:30 -0500

    implemented filter on list

--- src/components/CityList.js
+++ src/components/CityList.js
@@ -3,32 +3,22 @@ import { useState } from "react";
 import CityItem from './CityItem';
 
 export default function CityList({ data }) {
-    const getItem = (data, index) => ({
-      id: data[index]._id.toString(), // Convert to string
-      country: data[index].country,
-      name: data[index].name,
-      coord: data[index].coord
-    });
-  
-    const getItemCount = (data) => data.length;
-  
-    return (
-      <VirtualizedList
-        data={data}
-        initialNumToRender={10}
-        renderItem={({ item }) => (
-          <CityItem 
-            key={item.id}  // Add key prop here
-            name={item.name}
-            country={item.country}
-            coord={item.coord}
-          />
-        )}
-        keyExtractor={item => item.id.toString()}  // Ensure string
-        getItemCount={getItemCount}
-        getItem={getItem}
-        maxToRenderPerBatch={10}
-        windowSize={5}
-      />
-    );
-  }
+  const getItem = (data, index) => data[index];
+
+  return (
+    <VirtualizedList
+      data={data}
+      initialNumToRender={10}
+      renderItem={({ item }) => (
+        <CityItem 
+          name={item.name}
+          country={item.country}
+          coord={item.coord}
+        />
+      )}
+      keyExtractor={item => item._id.toString()}
+      getItemCount={data => data.length}
+      getItem={getItem}
+    />
+  );
+}
